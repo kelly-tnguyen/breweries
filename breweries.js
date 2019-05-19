c905feed5fbddbf33b610699ca9fba000aa41a2e
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-05-18 23:07:20 -0500

    still in progress

--- breweries.js
+++ breweries.js
@@ -9,37 +9,39 @@ const rl = readline.createInterface({
 });
 
 function getPrompt() {
-  rl.question('Zipcode: ', (zip) => {
+  rl.question(`Zipcode: `, (zip) => {
   console.log( getZip(zip) );
   getPrompt();
 });
 }
 getPrompt();
-
 let userInput = [];
 
 
 function getZip() {
 const baseURL = 'https://sandbox-api.brewerydb.com/v2/';
 
-    fetch(baseURL + 'locations/?key=' + process.env.KEY + '&postalCode=' + userInput) 
+    fetch(baseURL + 'locations/?key=' + process.env.KEY + '&postalCode= ' + userInput) 
     .then(res => {
       if(!res.ok) {
         throw Error(res.statusText)
       } return res.json()
     })
     .then(obj => {
+
         const totalResults = obj.totalResults;
-        const data = obj.data[0]; //Make this more dynamic in case there are like 5 breweries around
+        const data = obj.data[1]; //Make this more dynamic in case there are like 5 breweries around
 
         const breweryName = data.name;
         const breweryAddress = data.streetAddress;
         const breweryPhone = data.phone;
+        const breweryWebsite = data.website;
 
         console.log(`There are ${totalResults} results in that zip code!`);
         console.log(breweryName);
         console.log(breweryAddress);
         console.log(breweryPhone);
+        console.log(breweryWebsite);
 
     })
 
